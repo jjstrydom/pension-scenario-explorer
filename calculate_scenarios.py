30fef799863fc741dcde655495cce07e388bdbb0
Jurgen Strydom <jurgen.strydom@gmail.com>
2024-02-18 21:30:47 +0200

    refactoring scenario number cunching

--- calculate_scenarios.py
+++ calculate_scenarios.py
@@ -25,6 +25,30 @@ def calculate_parameters_monthly(parameters_yearly: dict) -> dict:
     return parameters_monthly
 
 
+def calculate_scenarios(
+            periods: int, 
+            drawdown_start_period: int, 
+            parameters_monthly: dict, 
+            amount_starting: int | float,
+            contribution_monthly: int | float,
+            retirement_drawdown_monthly: int | float,
+            number_of_runs=1000
+        ) -> np.array:
+    size = (periods, number_of_runs)
+    inflation = np.random.normal(parameters_monthly['inflation_mean'], parameters_monthly['inflation_std'], size=size)
+    growth = np.random.normal(parameters_monthly['growth_mean'], parameters_monthly['growth_std'], size=size)
+    delta_growth = growth - inflation
+    scenarios = np.ones(size)
+    scenarios[0,:] = amount_starting
+    for p in range(periods)[1:]:
+        if drawdown_start_period > p:
+            scenarios[p,:] = (scenarios[p-1,:] + contribution_monthly) * (1+delta_growth[p,:])
+        else:
+            scenarios[p,:] = (scenarios[p-1,:] - retirement_drawdown_monthly) * (1+delta_growth[p,:])
+    scenarios[scenarios < 0] = 0
+    return scenarios
+
+
 def plot_scenarios(scenarios: np.array, age_starting: int, age_sim_end: int, percentiles: list = [1,10,25,50,75,90,99]) -> None:
     scenario_stats = np.percentile(scenarios, percentiles, axis=1)
     plot_data = pd.DataFrame(scenario_stats.T, columns = [f"{p}" for p in percentiles])
@@ -35,27 +59,24 @@ def plot_scenarios(scenarios: np.array, age_starting: int, age_sim_end: int, per
     fig.write_html("result.html")
 
 
-def calculate_scenarios():
+def main():
     config = load_configs()
     config = config['person1']  # TODO: loop through multiple
     periods, drawdown_start_period = calculate_transition_dates(config['age_sim_end'], config['age_starting'], config['age_retirement'])
     # config['growth_std'] = config['growth_std']/np.sqrt(12)  # fact sheets shows annulaised standard devivation...
     parameters_monthly = calculate_parameters_monthly(config)
-
-    size = (periods, config['number_of_runs'])
-    inflation = np.random.normal(parameters_monthly['inflation_mean'], parameters_monthly['inflation_std'], size=size)
-    growth = np.random.normal(parameters_monthly['growth_mean'], parameters_monthly['growth_std'], size=size)
-    delta_growth = growth - inflation
-    scenarios = np.ones(size)
-    scenarios[0,:] = config['amount_starting']
-    for p in range(periods)[1:]:
-        if drawdown_start_period > p:
-            scenarios[p,:] = (scenarios[p-1,:] + config['contribution_monthly']) * (1+delta_growth[p,:])
-        else:
-            scenarios[p,:] = (scenarios[p-1,:] - config['retirement_drawdown_monthly']) * (1+delta_growth[p,:])
-    scenarios[scenarios < 0] = 0
+    scenarios = calculate_scenarios(
+            periods,
+            drawdown_start_period,
+            parameters_monthly,
+            config['amount_starting'],
+            config['contribution_monthly'],
+            config['retirement_drawdown_monthly'],
+            config['number_of_runs']
+        )
+    
     plot_scenarios(scenarios, config['age_starting'], config['age_sim_end'])
 
 
 if __name__ == '__main__':
-    calculate_scenarios()
+    main()
